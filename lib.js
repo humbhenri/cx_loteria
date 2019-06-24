1b96ebe3face0cd0b430e5c4edd43229d83cf1d3
Humberto Pinheiro <humbhenri@gmail.com>
2019-06-24 17:18:37 -0300

    setup mock

--- lib.js
+++ lib.js
@@ -3,17 +3,14 @@ const cheerio = require('cheerio');
 const url = 'https://www.sorteonline.com.br/todas/resultados';
 
 function buscaUltimosResultados() {
-    const options = {
-        url: url,
-        transform: function (body) { return cheerio.load(body); }
-    };
-    return rp(options).then(function ($) {
+    return rp(url).then(function(html) {
         const resultados = [];
+        const $ = cheerio.load(html);
         $('.DivDeVisibilidade').each((_, element) => {
             const cheerioElement = $(element);
             resultados.push({
                 nome: cheerioElement.find('.name').text(),
-                premio: cheerioElement.find('.premio').text(),
+                premio: cheerioElement.find('.premio').text()
             });
         });
         return resultados;
--- lib.test.js
+++ lib.test.js
@@ -1,7 +1,13 @@
 const buscaUltimosResultados = require('./lib');
+const rp = require('request-promise');
+const fs = require('fs');
+const siteMock = fs.readFileSync('./mock.html', 'utf-8');
+
+jest.mock('request-promise');
+rp.mockResolvedValue(siteMock);
 
 test('retorna alguma coisa', () => {
     return buscaUltimosResultados().then(data => {
         expect(data).toBeTruthy();
-    })
-})
+    });
+});
