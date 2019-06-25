bdae1d27941541a5e1098271dea077632a09b799
Humberto <humbhenri@gmail.com>
2019-06-25 15:58:48 -0300

    adicioana teste que faz chamada real

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 [![npm (scoped)](https://img.shields.io/npm/v/@humbhenri/cx_loteria.svg)](https://github.com/humbhenri/cx_loteria)
 
+Work In Progress
 
 API dos resultados das loterias da Caixa.
 
---
+++ index.real.test.js
@@ -0,0 +1,13 @@
+const buscaUltimosResultados = require('./index');
+
+test('Deve retornar todos os resultados das loterias', () =>
+    buscaUltimosResultados().then(resultado => {
+        const loterias = Object.keys(resultado);
+        expect(loterias.length).toBe(10);
+        for (let loteria of loterias) {
+            expect(resultado[loteria].nome).not.toBeNull();
+            expect(resultado[loteria].premio).toMatch(/R\$/);
+            expect(resultado[loteria].dataSorteio).not.toBeNull();
+            expect(resultado[loteria].numConcurso).not.toBeNull();
+        }
+    }));
