8db34cadaecfb65ca41bb456cea33c1d776c682a
Humberto Pinheiro <humbhenri@gmail.com>
2019-06-25 12:08:56 -0300

    resultados de todas loterias

--- index.js
+++ index.js
@@ -2,72 +2,104 @@ const rp = require('request-promise');
 const cheerio = require('cheerio');
 const url = 'https://www.sorteonline.com.br/todas/resultados';
 
+const loteriaNomeSel = '.name';
+const premioSel = '.premio.color.bold';
+const dataSorteioSel = '.color.header-resultados__datasorteio';
+const resultadoSel = '.result.result-default.center > ul';
+const numConcursoSel = '.color.header-resultados__nro-concurso';
+const duplaSena = 'Dupla Sena';
+const timemania = 'Timemania';
+const timemaniaSel = '.time-coracao';
+const diaSorte = 'Dia de Sorte';
+const diaSorteSel = '.time-coracao';
+const loteriaFederal = 'Loteria Federal';
+const loteriaFederalSel = 'ul.federal > li';
+const loteriaFederalNum = 'div.bg';
+const loteca = 'Loteca';
+const lotecaSel = 'table.result-loteca tr';
+const lotogol = 'Lotogol';
+const lotogolSel = 'table.result-lotogol tr';
+const loterialSel = '.DivDeVisibilidade';
+
 function buscaUltimosResultados() {
     return rp(url).then(function(html) {
         const resultados = {};
         const $ = cheerio.load(html);
-        $('.DivDeVisibilidade').each((_, element) => {
+        $(loterialSel).each((_, element) => {
             const cheerioElement = $(element);
-            const nome = cheerioElement.find('.name').text();
+            const nome = cheerioElement.find(loteriaNomeSel).text();
             resultados[nome] = {
                 nome: nome,
-                premio: cheerioElement.find('.premio.color.bold').text(),
+                premio: cheerioElement.find(premioSel).text(),
                 dataSorteio: cheerioElement
-                    .find('.color.header-resultados__datasorteio')
+                    .find(dataSorteioSel)
                     .text()
                     .trim(),
                 resultado: cheerioElement
-                    .find('.result.result-default.center > ul')
+                    .find(resultadoSel)
                     .text()
                     .split(/\s+/)
                     .filter(s => s != ''),
                 numConcurso: cheerioElement
-                    .find('.color.header-resultados__nro-concurso')
+                    .find(numConcursoSel)
                     .text()
                     .trim()
             };
             const loteria = resultados[nome];
             switch (nome) {
-                case 'Dupla Sena':
+                case duplaSena:
                     loteria.resultadoDuplaSena1 = loteria.resultado.slice(0, 6);
                     loteria.resultadoDuplaSena2 = loteria.resultado.slice(6);
+                    delete loteria.resultado;
                     break;
-                case 'Timemania':
+                case timemania:
                     loteria.timeCoracao = cheerioElement
-                        .find('.time-coracao')
-                        .text();
-                    break;
-                case 'Dia de Sorte':
-                    loteria.mes = cheerioElement
-                        .find('.time-coracao')
+                        .find(timemaniaSel)
                         .text()
                         .trim();
                     break;
-                case 'Loteria Federal':
-                    cheerioElement.find('ul.federal > li').each((_, li) => {
+                case diaSorte:
+                    loteria.mes = cheerioElement
+                        .find(diaSorteSel)
+                        .text()
+                        .trim();
+                    break;
+                case loteriaFederal:
+                    cheerioElement.find(loteriaFederalSel).each((_, li) => {
                         const cheerioLi = $(li);
                         loteria.resultado.push(
                             cheerioLi
-                                .find('div.bg')
+                                .find(loteriaFederalNum)
                                 .text()
                                 .trim()
                         );
                     });
                     break;
-                case 'Loteca':
-                    cheerioElement
-                        .find('table.result-loteca tr')
-                        .each((_, tr) => {
-                            const jogo = $(tr)
-                                .text()
-                                .split(/\s+/);
-                            loteria.resultado.push({
-                                casa: jogo[2],
-                                casaGols: jogo[3],
-                                visitanteGols: jogo[5],
-                                visitante: jogo[6]
-                            });
+                case loteca:
+                    cheerioElement.find(lotecaSel).each((_, tr) => {
+                        const jogo = $(tr)
+                            .text()
+                            .split(/\s+/);
+                        loteria.resultado.push({
+                            casa: jogo[2],
+                            casaGols: jogo[3],
+                            visitanteGols: jogo[5],
+                            visitante: jogo[6]
                         });
+                    });
+                    break;
+                case lotogol:
+                    cheerioElement.find(lotogolSel).each((_, tr) => {
+                        const jogo = $(tr)
+                            .text()
+                            .split(/\s+/);
+                        loteria.resultado.push({
+                            casa: jogo[2],
+                            casaGols: jogo[3],
+                            visitanteGols: jogo[5],
+                            visitante: jogo[6]
+                        });
+                    });
                     break;
             }
         });
