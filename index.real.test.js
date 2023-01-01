1a8e866b01769fcae53fa06a55904f1b1d905b62
Humberto Henrique Campos Pinheiro <humbhenri@gmail.com>
2023-01-01 15:43:29 -0300

    fix tests

--- index.real.test.js
+++ index.real.test.js
@@ -3,7 +3,7 @@ const buscaUltimosResultados = require('./index');
 test('Deve retornar todos os resultados das loterias', () =>
     buscaUltimosResultados().then(resultado => {
         const loterias = Object.keys(resultado);
-        expect(loterias.length).toBe(10);
+        expect(loterias.length).toBe(12);
         for (let loteria of loterias) {
             expect(resultado[loteria].nome).not.toBeNull();
             expect(resultado[loteria].premio).toMatch(/R\$/);
